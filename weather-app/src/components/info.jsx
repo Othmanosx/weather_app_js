9381c2b3cb4dd1adea7f90114c5918c0486c58d4
“Othman <othmanosx@gmail.com>
2021-02-09 19:08:12 +0300

    create components

---
+++ weather-app/src/components/info.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function more() {
+    return (
+        <div>
+            more informations about weather
+        </div>
+    )
+}
---
+++ weather-app/src/components/today.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function today() {
+    return (
+        <div>
+            today view
+        </div>
+    )
+}
