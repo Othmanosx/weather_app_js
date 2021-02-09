f37e4f955ec7aedb8b1fdfe76cc02bf78d012706
“Othman <othmanosx@gmail.com>
2021-02-09 19:13:09 +0300

    organize components

--- weather-app/src/App.js
+++ weather-app/src/App.js
@@ -1,10 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import Container from './components/container'
 
 function App() {
   return (
     <div className="App">
       Weather app
+      <Container />
     </div>
   );
 }
--- weather-app/src/components/container.jsx
+++ weather-app/src/components/container.jsx
@@ -1,10 +1,18 @@
 import React, { Component } from 'react'
+import Chart from './chart'
+import Info from './info'
+import SearchBar from './searchBar'
+import Today from './today'
 
 export default class container extends Component {
     render() {
         return (
             <div>
                 container
+                <SearchBar />
+                <Info />
+                <Chart />
+                <Today />
             </div>
         )
     }
