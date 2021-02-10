f3bbda8e862caeb55df1d51ba1cfe9c8ac2f5628
AREEG94FAHAD <fahedareeg@gmail.com>
2021-02-10 09:47:19 -0800

    Feat add functions to display sun_set and sun_rise

--- weather-app/src/components/today.jsx
+++ weather-app/src/components/today.jsx
@@ -1,35 +1,37 @@
-import React from 'react'
-import './today.css'
+import React from "react";
+import "./today.css";
 
 export default function today(props) {
-
-    return (
-        <div>
-            <h1>Forcast for {props.state.cityName}</h1>
-            <div class="widget">
-
-                <div class="left-panel panel">
-                    <div class="date">
-                        {props.state.date[0]} {props.state.data[0].applicable_date}
-                        <br />
-
-                    </div>
-                    <div class="city">
-                        {props.state.cityName}
-                    </div>
-                    <div class="temp">
-                        <img src="https://s5.postimg.cc/yzcm7htyb/image.png" alt="" width="30" />
-                        {Math.floor(props.state.the_temp[0])}&deg;
-                </div>
-                </div>
-                <div class="right-panel panel">
-                    {props.state.data[0].weather_state_name}
-                </div>
-
-
-            </div>
-
-
+  function convertTime(date) {
+    const event = new Date(date);
+    return event.toLocaleTimeString("en-US");
+  }
+  return (
+    <div>
+      <h1>Forcast for {props.state.title}</h1>
+      <div class="widget">
+        <div class="left-panel panel">
+          <div class="date">
+            {props.state.date[0]} {props.state.data.applicable_date}
+            <br />
+          </div>
+          <div class="city">{props.state.title}</div>
+          <div class="temp">
+            <img
+              src="https://s5.postimg.cc/yzcm7htyb/image.png"
+              alt=""
+              width="30"
+            />
+            {Math.floor(props.state.the_temp[0])}&deg;
+          </div>
         </div>
-    )
+        <div class="right-panel panel">
+          {props.state.data.weather_state_name} <br></br>
+          sun rise {convertTime(props.state.sun_rise)}
+          <br></br> Sun set {convertTime(props.state.sun_set)}
+        </div>
+      </div>
+    </div>
+  );
 }
+
